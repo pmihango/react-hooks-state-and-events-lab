6468cf2ca68d019ff69fa0db46f344736d00d2f4
pmihango <pmihango@gmail.com>
2023-01-24 10:03:57 +0300

    completed lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,22 @@
 import React from "react";
+import {useState} from "react";
 
 function Item({ name, category }) {
+
+  const [add, setIsAdd] = useState(false);
+  function handleClick() {
+    setIsAdd((add) => !add)
+  }
+
+  const liClass = add ? "in-cart" : ""
+  const addClass = add ? "remove" : "add"
+
   return (
-    <li className="">
+    <li className={liClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={addClass} onClick={handleClick}>Add to Cart</button>
     </li>
   );
 }
-
 export default Item;
